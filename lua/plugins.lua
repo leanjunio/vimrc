87474a07035000af0848ad75f48d1f65c9e057b9
leanjunio <lean.junio@outlook.com>
2021-06-24 09:25:22 -0400

    reformat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,44 +2,44 @@ local packer = require'packer'
 local use = packer.use
 
 return packer.startup(function()
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = 'TSUpdate',
-		config = function() require 'plugins.treesitter' end
-	}
-	use {
-		'airblade/vim-rooter',
-		config = function()
-			vim.g.rooter_patterns = { '.git/', '.vim/' }
-		end
-	}
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = 'TSUpdate',
+    config = function() require 'plugins.treesitter' end
+  }
+  use {
+    'airblade/vim-rooter',
+    config = function()
+      vim.g.rooter_patterns = { '.git/', '.vim/' }
+    end
+  }
 
-	-- LSP Configurations for the builtin LSP Client
-	use {
-		'neovim/nvim-lspconfig',
-		config = function() require 'plugins.nvim-lspconfig' end
-	}
+  -- LSP Configurations for the builtin LSP Client
+  use {
+    'neovim/nvim-lspconfig',
+    config = function() require 'plugins.nvim-lspconfig' end
+  }
 
-	-- LSP Completion
-	use {
-		'hrsh7th/nvim-compe',
-		config = function() require 'plugins.nvim-compe' end
-	}
+  -- LSP Completion
+  use {
+    'hrsh7th/nvim-compe',
+    config = function() require 'plugins.nvim-compe' end
+  }
 
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, { 'nvim-telescope/telescope-fzy-native.nvim' }},
-		config = function() require 'plugins.telescope' end
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, { 'nvim-telescope/telescope-fzy-native.nvim' }},
+    config = function() require 'plugins.telescope' end
 
-	}
-	use {
-		'hoob3rt/lualine.nvim',
-		requires = {'kyazdani42/nvim-web-devicons', opt = true},
-		config = function() require 'plugins.lualine' end
-	}	
-	use 'tpope/vim-fugitive'
-	use 'nvim-lua/lsp-status.nvim'
-	use 'danilo-augusto/vim-afterglow'
+  }
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true},
+    config = function() require 'plugins.lualine' end
+  }	
+  use 'tpope/vim-fugitive'
+  use 'nvim-lua/lsp-status.nvim'
+  use 'danilo-augusto/vim-afterglow'
 
 end)
--- plugin/packer_compiled.vim
+++ plugin/packer_compiled.vim
@@ -137,6 +137,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\n1\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\22plugins.telescope\frequire\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
+-- Config for: nvim-lspconfig
+time([[Config for nvim-lspconfig]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27plugins.nvim-lspconfig\frequire\0", "config", "nvim-lspconfig")
+time([[Config for nvim-lspconfig]], false)
 -- Config for: lualine.nvim
 time([[Config for lualine.nvim]], true)
 try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20plugins.lualine\frequire\0", "config", "lualine.nvim")
@@ -153,14 +161,6 @@ time([[Config for nvim-compe]], false)
 time([[Config for vim-rooter]], true)
 try_loadstring("\27LJ\2\nA\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\3\0\0\n.git/\n.vim/\20rooter_patterns\6g\bvim\0", "config", "vim-rooter")
 time([[Config for vim-rooter]], false)
--- Config for: telescope.nvim
-time([[Config for telescope.nvim]], true)
-try_loadstring("\27LJ\2\n1\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\22plugins.telescope\frequire\0", "config", "telescope.nvim")
-time([[Config for telescope.nvim]], false)
--- Config for: nvim-lspconfig
-time([[Config for nvim-lspconfig]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27plugins.nvim-lspconfig\frequire\0", "config", "nvim-lspconfig")
-time([[Config for nvim-lspconfig]], false)
 if should_profile then save_profiles() end
 
 END
